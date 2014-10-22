ca91aa5924e7908af5f68da95f083815968d57c1
Viktor Kalinchuk <admin@Mac-Admin.local>
2014-10-22 08:18:30 +0300

    Initial Commit

---
+++ megapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  megapp
+//
+//  Created by Viktor Kalinchuk on 10/22/14.
+//  Copyright (c) 2014 Viktor Kalinchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ megapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  megapp
+//
+//  Created by Viktor Kalinchuk on 10/22/14.
+//  Copyright (c) 2014 Viktor Kalinchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
